365d1157c060c4331e9afc51efd36b4c47b136ae
Justin Mulroney <justin@theryna.com>
2023-08-15 11:31:02 -0400

    set up repo

---
+++ server/fileParser.js
@@ -0,0 +1,71 @@
+const fs = require('fs');
+const path = require('path');
+
+const folderPath = '/Users/justinmulroney/Documents/transcripts';
+let counter = 0;
+const readFilesInFolder = async(folderPath) => {
+
+  try {
+    const fileNames = await fs.promises.readdir(folderPath);
+    const textArray = [];
+
+    for (const fileName of fileNames) {
+      const filePath = path.join(folderPath, fileName);
+      const stats = await fs.promises.stat(filePath);
+
+      if (stats.isFile()) {
+        try {
+          const fileContent = await fs.promises.readFile(filePath, 'utf-8');
+
+          const chunkSize = 3000;
+          const overlap = 500;
+
+          let startIndex = 0;
+
+          while (startIndex < fileContent.length) {
+            counter += 1;
+            let endIndex = startIndex + chunkSize;
+
+            // Move the endIndex back to complete the last word if needed
+            if (endIndex < fileContent.length) {
+              while (endIndex > startIndex + overlap && !/\s/.test(fileContent.charAt(endIndex))) {
+                endIndex--;
+              }
+            }
+
+            // Include the last word if endIndex is at the end of a word
+            while (endIndex < fileContent.length && /\s/.test(fileContent.charAt(endIndex))) {
+              endIndex++;
+            }
+
+            // Move the startIndex to the beginning of the word if necessary
+            while (startIndex > 0 && !/\s/.test(fileContent.charAt(startIndex - 1))) {
+              startIndex--;
+            }
+
+            const chunk = fileContent.substring(startIndex, endIndex);
+            const chunkWithoutNewlines = chunk.replace(/\n/g, ''); // Remove newlines
+            textArray.push(chunkWithoutNewlines);
+            startIndex += chunkSize - overlap;
+          }
+        } catch (error) {
+          console.error(`Error reading file ${filePath}:`, error);
+        }
+      }
+    }
+
+    return textArray;
+  } catch (error) {
+    console.error('Error reading files:', error);
+    return [];
+  }
+};
+
+readFilesInFolder(folderPath)
+  .then((textArray) => {
+    console.log(textArray);
+    console.log(counter);
+  })
+  .catch((error) => {
+    console.error('An error occurred:', error);
+  });
---
+++ server/index.js
@@ -0,0 +1,44 @@
+
+
+const express = require("express");
+require('dotenv').config();
+
+// const PORT = process.env.PORT || 3001;
+
+// const app = express();
+const fetch = require("node-fetch");
+// app.get("/api", (req, res) => {
+//   res.json({ message: "test" });
+// });
+
+// app.listen(PORT, () => {
+//   console.log(`Server listening on ${PORT}`);
+// });
+
+const openAiHeaders = {
+  'Content-Type': 'application/json',
+  'Authorization':`Bearer ${process.env.OPEN_API_KEY}`
+};
+
+
+
+// const createEmbedding = async(textToEmbed)=> {
+//   let response = await fetch(`https://api.openai.com/v1/embeddings`, {
+//     method: 'POST',
+//     headers: openAiHeaders,
+//     body: JSON.stringify({
+//       'model': 'text-embedding-ada-002',
+//       'input': textToEmbed
+//     }),
+//   });
+//   if (response.ok) {
+//     response.json().then(data=>{
+//       console.log(data);
+//       return data;
+//     });
+//   } else {
+//     console.log('this is res', response);
+//   }
+// };
+
+// createEmbedding('apple stock');
